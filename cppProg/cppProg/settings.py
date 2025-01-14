6999efabf000696be5a905a8ba7b292d710f1e64
skewch <a.3066@mail.ru>
2025-01-14 11:51:58 +0500

    edit django project settings

--- cppProg/cppProg/settings.py
+++ cppProg/cppProg/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kn!vy*oqosa=4(k4$!b3rtr%iu++tm8lo_^0gco3enz5rpm2_u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 USE_I18N = True
 
