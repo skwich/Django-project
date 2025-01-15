931d95180ae134ced8c60482611244064d455509
skewch <a.3066@mail.ru>
2025-01-15 21:56:37 +0500

    create funcs for static paths

--- cppProgSite/views.py
+++ cppProgSite/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def statistics(request):
+    return render(request, 'statistics.html')
+
+def relevance(request):
+    return render(request, 'relevance.html')
+
+def geography(request):
+    return render(request, 'geography.html')
+
+def skills(request):
+    return render(request, 'skills.html')
+
+def last_vacs(request):
+    return render(request, 'last_vacs.html')
+
